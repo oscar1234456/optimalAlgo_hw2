ef4e402287c75adf958ee8b749fa115d2d7e9f1f
陳泰元 <chen.oscar@hotmail.com>
2021-04-04 18:41:17 +0800

    [Add]ADd pop_size, pc, pm and round fitness

--- GA.m
+++ GA.m
@@ -1,15 +1,19 @@
-x = linspace(-1.5, 4, 50);	% 在 x 軸 [-1.5,4] 之間取 50 點  
-y = linspace(-3, 3, 50);	% 在 y 軸 [-3,3] 之間取 50 點  
-[xx, yy] = meshgrid(x, y);	% xx 和 yy 都是 50x50 的矩陣  
-zz = sin(xx+yy)+((xx-yy).^2)-1.5.*xx+2.5.*yy+1;	% 計算函數值，zz 也是 50x50 的矩陣
-mesh(xx, yy, zz);		% 畫出立體網狀圖 
-% colormap(zeros(1,3));		% 以黑色呈現   
+% x = linspace(-1.5, 4, 50);	% 在 x 軸 [-1.5,4] 之間取 50 點  
+% y = linspace(-3, 3, 50);	% 在 y 軸 [-3,3] 之間取 50 點  
+% [xx, yy] = meshgrid(x, y);	% xx 和 yy 都是 50x50 的矩陣  
+% zz = sin(xx+yy)+((xx-yy).^2)-1.5.*xx+2.5.*yy+1;	% 計算函數值，zz 也是 50x50 的矩陣
+% mesh(xx, yy, zz);		% 畫出立體網狀圖 
+% % colormap(zeros(1,3));		% 以黑色呈現   
 
+pop_size = 20;
+pc = 0.25;
+pm = 0.01;
 
-ss = getNewGene(20);
-for i=1:length(ss)
+thisGene = getNewGene(pop_size);
+for i=1:length(thisGene)
     fprintf("now: %d \n",i);
-    vec = convGene2Value(ss{i});
+    vec = convGene2Value(thisGene{i});
     fprintf("X: %d \n",round(vec(1)));
     fprintf("Y: %d \n",round(vec(2)));
+    fprintf("fit: %d \n",round(fitness(vec(1), vec(2))));
 end
