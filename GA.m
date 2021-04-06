089641f9d525adfe102cde6f39596e05e4927c5f
陳泰元 <chen.oscar@hotmail.com>
2021-04-06 12:18:38 +0800

    [Add]Add crossover

--- GA.m
+++ GA.m
@@ -8,23 +8,38 @@
 pop_size = 20;
 pc = 0.25;
 pm = 0.01;
-fitnessRec = zeros(1,20);
+fitnessRec1 = zeros(1,20);
+fitnessRec2 = zeros(1,20);
+fitnessRec3 = zeros(1,20);
 thisGene = getNewGene(pop_size);
+%evaluate phase 1
 for i=1:length(thisGene)
     fprintf("now: %d \n",i);
     vec = convGene2Value(thisGene{i});
     fprintf("X: %4.3f \n",roundn(vec(1), -3));
     fprintf("Y: %4.3f \n",roundn(vec(2), -3));
     fprintf("fit: %5.4f \n",roundn(fitness(vec(1), vec(2)), -4));
-    fitnessRec(i) = roundn(fitness(vec(1), vec(2)), -4);
+    fitnessRec1(i) = roundn(fitness(vec(1), vec(2)), -4);
 end
-[newGene, pickRecord] = selectionProcess(thisGene);
+
+[newGene, pickRecord] = selectionProcess(thisGene); %pick gene
+%evaluate again 2
 for i=1:length(newGene)
     fprintf("now: %d \n",i);
     vec = convGene2Value(newGene{i});
     fprintf("X: %4.3f \n",roundn(vec(1), -3));
     fprintf("Y: %4.3f \n",roundn(vec(2), -3));
     fprintf("fit: %5.4f \n",roundn(fitness(vec(1), vec(2)), -4));
-    fitnessRec(i) = roundn(fitness(vec(1), vec(2)), -4);
+    fitnessRec2(i) = roundn(fitness(vec(1), vec(2)), -4);
 end
 
+newGene = crossoveer(newGene, pc); %crossover
+%evaluate again 3
+for i=1:length(newGene)
+    fprintf("now: %d \n",i);
+    vec = convGene2Value(newGene{i});
+    fprintf("X: %4.3f \n",roundn(vec(1), -3));
+    fprintf("Y: %4.3f \n",roundn(vec(2), -3));
+    fprintf("fit: %5.4f \n",roundn(fitness(vec(1), vec(2)), -4));
+    fitnessRec3(i) = roundn(fitness(vec(1), vec(2)), -4);
+end
